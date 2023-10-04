a75ba8b60c951f0ac77fdf56493bf563d1e1c12d
scott-gr <mscottgriffin@gmail.com>
2023-10-04 14:05:35 -0400

    created docs_sidebar_config

---
+++ apps/web/src/app/(docs)/docs/[[...slug]]/docs-sidebar-config.ts
@@ -0,0 +1,42 @@
+export const DOCS_SIDEBAR_CONFIG = [
+  {
+    title: "Calculations",
+    items: [
+      {
+        title: "Range",
+        href: "/docs/calculations/range",
+        items: [],
+      },
+      {
+        title: "Reload",
+        href: "/docs/calculations/reload",
+        items: [],
+      },
+    ],
+  },
+  {
+    title: "FAQ",
+    href: "/docs/faq",
+    items: [],
+  },
+  {
+    title: "Features",
+    items: [
+      {
+        title: "Compare",
+        href: "/docs/features/compare",
+        items: [],
+      },
+      {
+        title: "Roll Editor",
+        href: "/docs/features/roll-editor",
+        items: [],
+      },
+      {
+        title: "Search",
+        href: "/docs/features/search",
+        items: [],
+      },
+    ]
+  }
+]
